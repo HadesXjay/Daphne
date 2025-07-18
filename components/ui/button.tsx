7c52051bc8be7e35d7f6d2755e7c6afd1eaa666c
HadesXjay <jayvimp@gmail.com>
2025-07-18 17:26:53 +0800

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,7 @@
 export function Button({ children, className = '', ...props }) {
-  return <button className={`rounded px-4 py-2 font-semibold ${className}`} {...props}>{children}</button>;
+  return (
+    <button className={`rounded px-4 py-2 font-semibold ${className}`} {...props}>
+      {children}
+    </button>
+  );
 }
