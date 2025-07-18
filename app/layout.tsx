163e846b4dc1fe947b18c6f9549534d670377c25
HadesXjay <jayvimp@gmail.com>
2025-07-18 18:23:52 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
+// app/layout.tsx
 export const metadata = {
   title: "Daphne Condino Portfolio",
   description: "Junior Architect & Graphic Designer",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
