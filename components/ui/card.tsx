8b90a64972bca24a29c6cf2d783cce3753fc3012
HadesXjay <jayvimp@gmail.com>
2025-07-18 17:27:03 +0800

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,6 +1,11 @@
 export function Card({ children, className = '' }) {
-  return <div className={`rounded-lg shadow-lg bg-white dark:bg-gray-800 ${className}`}>{children}</div>;
+  return (
+    <div className={`rounded-lg shadow-lg bg-white dark:bg-gray-800 ${className}`}>
+      {children}
+    </div>
+  );
 }
+
 export function CardContent({ children, className = '' }) {
   return <div className={className}>{children}</div>;
 }
